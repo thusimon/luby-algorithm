55a0af365124593ac7e3faf8b2f078df403a826d
Lu Xu <thusimon@gmail.com>
2022-04-21 18:14:27 -0700

    Q5 pending

---
+++ src/com/luby/algo/questions/Q5.java
@@ -0,0 +1,21 @@
+package com.luby.algo.questions;
+
+public class Q5 {
+  class Solution {
+    /**
+     * Given a string s, return the longest palindromic substring in s
+     * Input: s = "babad"
+     * Output: "bab"
+     * Explanation: "aba" is also a valid answer.
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+
+    }
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
