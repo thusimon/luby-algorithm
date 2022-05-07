cf5c2aabe3687fad4555fe37f03499c06b833835
Lu Xu <thusimon@gmail.com>
2022-05-07 08:56:35 -0700

    Update Q33.java

--- src/com/luby/algo/questions/Q33.java
+++ src/com/luby/algo/questions/Q33.java
@@ -33,6 +33,7 @@ public class Q33 {
 
     //stop playing the legend of heros, get back to exercise!
     //lu, work hard!
+    //another cheat
     public int search(int[] nums, int target) {
       int start = 0;
       int end = nums.length - 1;
