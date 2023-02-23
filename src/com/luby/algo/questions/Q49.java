ceebfc68b388212a4355da8fdb8d3467cd3389d8
Lu Xu <thusimon@gmail.com>
2023-02-22 20:00:36 -0800

    try Q49

---
+++ src/com/luby/algo/questions/Q49.java
@@ -0,0 +1,45 @@
+package com.luby.algo.questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q49 {
+  class Solution {
+    /**
+     * Given an array of strings strs, group the anagrams together.
+     * You can return the answer in any order.
+     * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+     * typically using all the original letters exactly once.
+     * e.g
+     * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+     * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+     *
+     * a->0, z->25
+     */
+
+    private String computeKey(String s) {
+      int[] charBits = new int[26];
+      int a = 'a';
+      for(byte c : s.getBytes()) {
+        int cIdx = c - a;
+        charBits[cIdx]++;
+      }
+      return Arrays.stream(charBits).mapToObj(String::valueOf).collect(Collectors.joining(","));
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+      List<List<String>> result = new ArrayList<>();
+      for(String s : strs) {
+        String sKey = computeKey(s);
+        System.out.println(sKey);
+      }
+      return result;
+    }
+  }
+
+  public static void main(String[] args) {
+    Q49.Solution solution = new Q49().new Solution();
+    System.out.println(solution.groupAnagrams(new String[] {"ate", "eat", "aatz"}));
+  }
+}
