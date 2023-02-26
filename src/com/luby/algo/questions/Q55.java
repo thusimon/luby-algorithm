6037298444a5032b308454fd3f204dbac1a9adcb
Lu Xu <thusimon@gmail.com>
2023-02-26 14:39:08 -0800

    Q55 try

---
+++ src/com/luby/algo/questions/Q55.java
@@ -0,0 +1,21 @@
+package com.luby.algo.questions;
+
+public class Q55 {
+  class Solution {
+    /**
+     * You are given an integer array nums.
+     * You are initially positioned at the array's first index,
+     * and each element in the array represents your maximum jump length at that position.
+     * Return true if you can reach the last index, or false otherwise. e.g
+     * Input: nums = [2,3,1,1,4]
+     * Output: true
+     * Input: nums = [3,2,1,0,4]
+     * Output: false
+     * @param nums
+     * @return
+     */
+    public boolean canJump(int[] nums) {
+      return true;
+    }
+  }
+}
