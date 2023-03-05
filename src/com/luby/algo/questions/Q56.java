3b156a439e6cad2c1bcb1c6b12f16a47fce980d1
Lu Xu <thusimon@gmail.com>
2023-03-04 17:56:40 -0800

    try Q56

---
+++ src/com/luby/algo/questions/Q56.java
@@ -0,0 +1,20 @@
+package com.luby.algo.questions;
+
+public class Q56 {
+  class Solution {
+    /**
+     * Given an array of intervals where intervals[i] = [start_i, end_i],
+     * merge all overlapping intervals,
+     * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+     * e.g
+     * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+     * Output: [[1,6],[8,10],[15,18]]
+     * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+     * @param intervals
+     * @return
+     */
+    public int[][] merge(int[][] intervals) {
+
+    }
+  }
+}
