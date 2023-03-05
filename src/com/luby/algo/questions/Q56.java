27d1860b1374ce269253cb669b5beaaaccebeee1
Lu Xu <thusimon@gmail.com>
2023-03-05 15:23:49 -0800

    Q56

--- src/com/luby/algo/questions/Q56.java
+++ src/com/luby/algo/questions/Q56.java
@@ -1,5 +1,9 @@
 package com.luby.algo.questions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Q56 {
   class Solution {
     /**
@@ -14,7 +18,46 @@ public class Q56 {
      * @return
      */
     public int[][] merge(int[][] intervals) {
-
+      if (intervals.length <= 1) {
+        return intervals;
+      }
+      List<int[]> sorted = Arrays.stream(intervals).sorted((a, b) -> a[0] - b[0]).toList();
+      List<int[]> result = new ArrayList<>();
+      result.add(sorted.get(0));
+      for (int i = 1; i < sorted.size(); i++) {
+        int[] prev = result.get(result.size() - 1);
+        int[] curr = sorted.get(i);
+        if (curr[0] <= prev[1]) {
+          // should merge the current range
+          prev[1] = Math.max(prev[1], curr[1]);
+        } else {
+          result.add(curr);
+        }
+      }
+      return result.toArray(int[][]::new);
     }
+  }
+
+  private static void print(int[][] result) {
+    StringBuilder sb = new StringBuilder();
+    sb.append('[');
+    for(int[] a : result) {
+      sb.append(String.format("[%d, %d], ", a[0], a[1]));
+    }
+    sb.deleteCharAt(sb.length() - 1);
+    sb.deleteCharAt(sb.length() - 1);
+    sb.append("]");
+    System.out.println(sb.toString());
+  }
+  public static void main(String[] args) {
+    Q56.Solution s = new Q56().new Solution();
+    int[][] intervals = new int[][]{
+      new int[] {8, 10},
+      new int[] {2, 6},
+      new int[] {1, 3},
+      new int[] {15, 18},
+    };
+    int[][] result = s.merge(intervals);
+    print(result);
   }
 }
