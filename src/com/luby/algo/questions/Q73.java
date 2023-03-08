a786613ad550a485467e5f514fc5de5e9847f246
Lu Xu <thusimon@gmail.com>
2023-03-07 17:53:42 -0800

    Try Q73

---
+++ src/com/luby/algo/questions/Q73.java
@@ -0,0 +1,17 @@
+package com.luby.algo.questions;
+
+public class Q73 {
+  class Solution {
+    /**
+     * Given an m x n integer matrix matrix,
+     * if an element is 0, set its entire row and column to 0's.
+     * e.g
+     * Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+     * Output: [[1,0,1],[0,0,0],[1,0,1]]
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+
+    }
+  }
+}
