6f49e56d3aed54e5a81944afc69995f5d8abf47e
Lu Xu <thusimon@gmail.com>
2023-07-04 17:08:31 -0700

    Q79

--- src/com/luby/algo/questions/Q79.java
+++ src/com/luby/algo/questions/Q79.java
@@ -1,5 +1,7 @@
 package com.luby.algo.questions;
 
+import java.util.List;
+
 public class Q79 {
   /**
    * Given an m x n grid of characters board and a string word,
@@ -13,7 +15,54 @@ public class Q79 {
    */
   class Solution {
     public boolean exist(char[][] board, String word) {
+      for(int y = 0; y < board.length; y++) {
+        for (int x = 0; x < board[0].length; x++) {
+          if (existInternal(board, x, y, word, 0)) {
+            return true;
+          }
+        }
+      }
       return false;
     }
+
+    public boolean existInternal(char[][] board, int x, int y, String word, int curIdx) {
+      // reaches the end, return true
+      if (curIdx >= word.length()) {
+        return true;
+      }
+      // check boundary
+      int row = board.length;
+      int col = board[0].length;
+      if (x < 0 || x >= col || y < 0 || y >= row) {
+        return false;
+      }
+      char curChar = word.charAt(curIdx);
+      char temp = board[y][x];
+      if (board[y][x] == curChar) {
+        board[y][x] = '*';
+      } else {
+        return false;
+      }
+      boolean res = existInternal(board, x+1, y, word, curIdx+1)
+        || existInternal(board, x-1, y, word, curIdx+1)
+        || existInternal(board, x, y+1, word, curIdx+1)
+        || existInternal(board, x, y-1, word, curIdx+1);
+      if(!res) {
+        board[y][x] = temp;
+      }
+      return res;
+    }
+  }
+
+  public static void main(String[] args) {
+    Q79.Solution s = new Q79().new Solution();
+    char[][] board = new char[][]{
+      new char[]{'A', 'B', 'C', 'E'},
+      new char[]{'S', 'F', 'C', 'S'},
+      new char[]{'A', 'D', 'E', 'E'}
+    };
+    //System.out.println(s.exist(board, "ABCCED"));
+    //System.out.println(s.exist(board, "SEE"));
+    System.out.println(s.exist(board, "ABCB"));
   }
 }
