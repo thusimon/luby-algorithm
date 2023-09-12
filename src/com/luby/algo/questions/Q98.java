d777cfa2017b6df8a74bac09fa9b093d167866b0
Lu Xu <thusimon@gmail.com>
2023-09-11 20:22:48 -0700

    Update Q98.java

--- src/com/luby/algo/questions/Q98.java
+++ src/com/luby/algo/questions/Q98.java
@@ -2,6 +2,9 @@ package com.luby.algo.questions;
 
 import com.luby.algo.common.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q98 {
   class Solution {
     /**
@@ -19,11 +22,34 @@ public class Q98 {
       double val = (double)node.val;
       return val < max && val > min && isValidBSTNode(node.left, min, val) && isValidBSTNode(node.right, val, max);
     }
+
     public boolean isValidBST(TreeNode root) {
       if (root == null) {
         return false;
       }
       return isValidBSTNode(root, -Double.MAX_VALUE, Double.MAX_VALUE);
+    }
+
+    private void inorder(TreeNode node, List<Integer> list) {
+      if (node == null) {
+        return;
+      }
+      inorder(node.left, list);
+      list.add(node.val);
+      inorder(node.right, list);
+    }
+    public boolean isValidBST2(TreeNode root) {
+      // in-order traverse
+      List<Integer> list = new ArrayList<>();
+      inorder(root, list);
+      double min = -Double.MAX_VALUE;
+      for(int x : list) {
+        if (x <= min) {
+          return false;
+        }
+        min = x;
+      }
+      return true;
     }
   }
 }
