e422d07d538e4cbdaee128174fbe7fcb2fb64a62
Lu Xu <thusimon@gmail.com>
2023-09-13 18:17:08 -0700

    Create Q103.java

---
+++ src/com/luby/algo/questions/Q103.java
@@ -0,0 +1,17 @@
+package com.luby.algo.questions;
+
+import com.luby.algo.common.TreeNode;
+
+import java.util.List;
+
+public class Q103 {
+  class Solution {
+    /**
+     * Given the root of a binary tree, return the zigzag level order traversal of its nodes' values.
+     * (i.e., from left to right, then right to left for the next level and alternate between).
+     */
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+    }
+  }
+}
