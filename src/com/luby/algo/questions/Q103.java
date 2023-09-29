47865ab0069b7ad00800abfaa2b05c7780d0b950
Lu Xu <thusimon@gmail.com>
2023-09-28 18:20:51 -0700

    Update Q103.java

--- src/com/luby/algo/questions/Q103.java
+++ src/com/luby/algo/questions/Q103.java
@@ -12,7 +12,6 @@ public class Q103 {
      * (i.e., from left to right, then right to left for the next level and alternate between).
      */
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-
       return new ArrayList<>();
     }
   }
