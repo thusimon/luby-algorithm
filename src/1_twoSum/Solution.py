3224cea8f4c463e2926a4049483c89590b2c65f0
Lu Xu <thusimon@gmail.com>
2024-02-07 20:22:45 -0800

    Create Solution.py

---
+++ src/1_twoSum/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    dict = {}
+    for idx, n in enumerate(nums):
+      dict[idx] = n
+    for key in dict.keys()
+      
